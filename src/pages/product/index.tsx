2c0d3fbd95bce362d2a41ac9d63aaf207bf20c60
devrick <lhpsystems0376@gmail.com>
2024-08-01 11:15:20 -0300

    troquei o button cadastrar

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -178,7 +178,7 @@ export default function Product({ categoryList }: CategoryProps){
   )
 }
 
-export const getServerSideProps = canSSRAuth(async (ctx) => {
+/* export const getServerSideProps = canSSRAuth(async (ctx) => {
   const apliClient = setupAPIClient(ctx)
 
   const response = await apliClient.get('/category');
@@ -189,4 +189,13 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
       categoryList: response.data
     }
   }
+})*/
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+
 })
