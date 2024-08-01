691c9ef51d97664493a4c3b920c62c2c99f4e145
devrick <lhpsystems0376@gmail.com>
2024-08-01 11:18:04 -0300

    retornei o button cadastrar

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -178,7 +178,7 @@ export default function Product({ categoryList }: CategoryProps){
   )
 }
 
-/* export const getServerSideProps = canSSRAuth(async (ctx) => {
+/export const getServerSideProps = canSSRAuth(async (ctx) => {
   const apliClient = setupAPIClient(ctx)
 
   const response = await apliClient.get('/category');
@@ -189,13 +189,4 @@ export default function Product({ categoryList }: CategoryProps){
       categoryList: response.data
     }
   }
-})*/
-
-
-export const getServerSideProps = canSSRAuth(async (ctx) => {
-
-  return {
-    props: {}
-  }
-
 })
